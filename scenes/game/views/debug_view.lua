ba7c4ddce5faee30aea191602c7571918d2c6d8d
d954mas <d954mas@gmail.com>
2022-08-30 00:03:57 +0300

    get game

---
+++ scenes/game/views/debug_view.lua
@@ -0,0 +1,43 @@
+local COMMON = require "libs.common"
+local GUI = require "libs_project.gui.gui"
+
+local Base = require "scenes.game.views.base_view"
+
+---@class DebugView:BaseView
+local View = COMMON.class("DebugView", Base)
+
+function View:init(root_name)
+	Base.init(self, root_name)
+end
+
+function View:bind_vh()
+	self.vh = {
+
+	}
+
+	self.views = {
+		btn_get_game = GUI.ButtonScale(self.root_name .. "/btn_get_game"),
+		btn_create_game = GUI.ButtonScale(self.root_name .. "/btn_create_game"),
+	}
+end
+function View:init_gui()
+	Base.init_gui(self)
+	self.views.btn_get_game:set_input_listener(function()
+		roketo.contract_get_game()
+	end)
+	self.views.btn_create_game:set_input_listener(function()
+		--roketo.login()
+	end)
+end
+
+function View:update(dt)
+	Base.update(self, dt)
+end
+
+function View:on_input(action_id, action)
+	if (self.ignore_input) then return false end
+	if (self.views.btn_create_game:on_input(action_id, action)) then return true end
+	if (self.views.btn_get_game:on_input(action_id, action)) then return true end
+end
+
+return View
