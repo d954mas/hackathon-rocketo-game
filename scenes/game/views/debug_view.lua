773fef1e0109243afc37c6e890390d7b23eb2211
d954mas <d954mas@gmail.com>
2022-09-01 11:39:15 +0300

    remove some debug btns

--- scenes/game/views/debug_view.lua
+++ scenes/game/views/debug_view.lua
@@ -16,10 +16,6 @@ function View:bind_vh()
 	}
 
 	self.views = {
-		btn_get_game = GUI.ButtonScale(self.root_name .. "/btn_get_game"),
-		btn_create_game = GUI.ButtonScale(self.root_name .. "/btn_create_game"),
-		btn_get_games_list = GUI.ButtonScale(self.root_name .. "/btn_get_games_list"),
-		btn_get_games_active_list = GUI.ButtonScale(self.root_name .. "/btn_get_games_active_list"),
 		btn_stream_timestamp = GUI.ButtonScale(self.root_name .. "/btn_stream_timestamp"),
 		btn_stream_is_premium = GUI.ButtonScale(self.root_name .. "/btn_stream_is_premium"),
 		btn_stream_buy = GUI.ButtonScale(self.root_name .. "/btn_stream_buy"),
@@ -27,18 +23,6 @@ function View:bind_vh()
 end
 function View:init_gui()
 	Base.init_gui(self)
-	self.views.btn_get_game:set_input_listener(function()
-		roketo.contract_get_game(0)
-	end)
-	self.views.btn_create_game:set_input_listener(function()
-		roketo.contract_create_game(roketo.get_account_id(),"d954mas2.testnet",5)
-	end)
-	self.views.btn_get_games_list:set_input_listener(function()
-		roketo.contract_get_games_list(roketo.get_account_id())
-	end)
-	self.views.btn_get_games_active_list:set_input_listener(function()
-		roketo.contract_get_games_active_list(roketo.get_account_id())
-	end)
 
 	self.views.btn_stream_timestamp:set_input_listener(function()
 		roketo.stream_calculate_end_timestamp()
@@ -59,10 +43,6 @@ end
 
 function View:on_input(action_id, action)
 	if (self.ignore_input) then return false end
-	if (self.views.btn_create_game:on_input(action_id, action)) then return true end
-	if (self.views.btn_get_game:on_input(action_id, action)) then return true end
-	if (self.views.btn_get_games_list:on_input(action_id, action)) then return true end
-	if (self.views.btn_get_games_active_list:on_input(action_id, action)) then return true end
 	if (self.views.btn_stream_timestamp:on_input(action_id, action)) then return true end
 	if (self.views.btn_stream_is_premium:on_input(action_id, action)) then return true end
 	if (self.views.btn_stream_buy:on_input(action_id, action)) then return true end
