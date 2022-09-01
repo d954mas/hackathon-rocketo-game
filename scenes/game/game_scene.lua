c310f3e0694b8bf902ca54004064bf9fdb609306
d954mas <d954mas@gmail.com>
2022-09-01 22:16:01 +0300

    ui

--- scenes/game/game_scene.lua
+++ scenes/game/game_scene.lua
@@ -1,6 +1,5 @@
 local WORLD = require "world.world"
 local SM_ENUMS = require "libs.sm.enums"
-local CURTAINS = require "curtains.curtains"
 local BaseScene = require "libs.sm.scene"
 local ENUMS = require "world.enums.enums"
 local COMMON = require "libs.common"
@@ -13,22 +12,9 @@ end
 
 function Scene:transition(transition)
     if (transition == SM_ENUMS.TRANSITIONS.ON_HIDE) then
-        if (CURTAINS.state == CURTAINS.STATES.OPENED) then
-            CURTAINS:command_closed(true)
-            COMMON.coroutine_wait(0.1)
-            if(not self._input.skip_curtains) then
-                while (CURTAINS.state == CURTAINS.STATES.IN_PROGRESS) do coroutine.yield() end
-            end
-        end
+
     elseif (transition == SM_ENUMS.TRANSITIONS.ON_SHOW) then
-        while (CURTAINS.state == CURTAINS.STATES.IN_PROGRESS) do coroutine.yield() end
-        if (CURTAINS.state == CURTAINS.STATES.CLOSED) then
-            CURTAINS:command_open(true)
-            coroutine.yield()
-            coroutine.yield()
-            coroutine.yield()
-            while (CURTAINS.state == CURTAINS.STATES.IN_PROGRESS) do coroutine.yield() end
-        end
+
     end
 end
 
